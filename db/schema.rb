6734c8efe295b0366285fa36d4c0cb7b452a4b79
Léa von Muralt <lea.demuralt@gmail.com>
2015-03-05 15:38:14 +0000

    Add users in the DB with custom registrations_controller and devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228150346) do
+ActiveRecord::Schema.define(version: 20150305145224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,6 @@ ActiveRecord::Schema.define(version: 20150228150346) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "login"
-    t.boolean  "isAdmin"
     t.date     "birthday"
     t.datetime "created_at"
     t.datetime "updated_at"
