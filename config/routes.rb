d5db9c2968bf1ec2715818e40019058fefe87d00
Cyriaque Skrapits <cysoun@gmail.com>
2015-03-06 13:40:04 +0100

    Adapted Devise login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users , :controllers => { :registrations => "users/registrations"}
-	root 'welcome#index'
+	root 'player#show'
   resources:users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
