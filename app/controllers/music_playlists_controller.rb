8a6005b86aa823c656ae5d0e4abba47b86e5fb49
Léa von Muralt <lea.demuralt@gmail.com>
2015-04-09 21:03:46 +0000

    fixed issue 22 again

--- app/controllers/music_playlists_controller.rb
+++ app/controllers/music_playlists_controller.rb
@@ -1,14 +1,21 @@
 class MusicPlaylistsController < ApplicationController
+
     def new
         @musicPlaylist  = MusicPlaylist.new
     end
 
     def create
         @musicPlaylist = MusicPlaylist.new(get_params)
-        if @musicPlaylist.save
+        @permitPlaylist = Playlist.where("id=?", @musicPlaylist.playlist_id).first
+        @authorized = @permitPlaylist.user_id == current_user.id
+        if (@authorized)
+          if @musicPlaylist.save
             redirect_to root_path, notice: 'Added to the playlist!'
+          end
+        else
+          @musicPlaylist.destroy
+          redirect_to root_path, notice: 'It is not your playlist!'
         end
-
     end
 
     def show
@@ -25,5 +32,7 @@ class MusicPlaylistsController < ApplicationController
     def get_params
         params.require(:music_playlist).permit(:music_id,:playlist_id)
     end
+  
+   
 
 end
